ab23f97ba747cea3bee390357cd4367dc80b5e85
Daniel Worthington-Bodart <dan@bodar.com>
2011-09-30 07:12:12 +0100

    Initial import and rename

---
+++ src/com/googlecode/lazyparsec/misc/Invokable.java
@@ -0,0 +1,12 @@
+package com.googlecode.lazyparsec.misc;
+
+/**
+ * Either a constructor or a method.
+ * 
+ * @author Ben Yu
+ */
+interface Invokable {
+  Class<?> returnType();
+  Class<?>[] parameterTypes();
+  Object invoke(Object[] args) throws Throwable ;
+}
